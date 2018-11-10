19e94abb4015805fdba54f5853209329ce5bc730
omeedth <aothomas@bu.edu>
2018-11-10 17:38:40 -0500

    Smol Change

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
+
+    running = True
+
+    # Display website
+    run_site()
+
+    while running:
+        wait_for_message()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+def run_site():
+    return render_template('flask.html')
+
+def wait_for_message():
     # Start our response
     resp = MessagingResponse()
 
@@ -13,6 +31,3 @@ def sms_ahoy_reply():
     resp.message("Alex is a dummy boy.")
 
     return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
