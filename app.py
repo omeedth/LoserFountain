ad48d691a72a7793a076ddccde9c322b99fddf17
petegilb <petegilb@bu.edu>
2018-11-10 17:54:43 -0500

    added functions above app declaration

--- app.py
+++ app.py
@@ -2,24 +2,6 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import render_template
 
-app = Flask(__name__)
-
-@app.route("/", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-
-    running = True
-
-    # Display website
-    run_site()
-
-    while running:
-        wait_for_message()
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 def run_site():
     return render_template('flask.html')
 
@@ -31,3 +13,22 @@ def wait_for_message():
     resp.message("Alex is a dummy boy.")
 
     return str(resp)
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+
+    running = True
+
+    # Display website
+    run_site()
+
+    while running:
+        return wait_for_message()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
