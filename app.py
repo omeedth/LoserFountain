0804334d7f1ca748a7270f427e589d2d685f543c
omeedth <aothomas@bu.edu>
2018-11-10 15:16:24 -0500

    Flask and Domain Working

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
 from flask import Flask
+from flask import render_template
+
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def hello(name=None):
+    return render_template('flask.html', name=name)
