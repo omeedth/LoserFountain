71c56d429b9790eb49a1c29e553d88cda3edd28a
petegilb <petegilb@bu.edu>
2018-11-10 16:47:34 -0500

    copied test code from twilio

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
-@app.route("/")
-def goodbye():
-    return "Hello World..."
-def hello():
-    return "Goodbye World..."
+@app.route("/", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Alex is a dummy boy.")
+
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
