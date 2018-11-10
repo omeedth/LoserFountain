7ab9fdfd0b4b59bec225440e01d0de7dc64b3703
petegilb <petegilb@bu.edu>
2018-11-10 16:36:54 -0500

    got rid of website aspect

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def goodbye():
+    return "Hello World..."
 def hello():
-    return "Hello World!"
+    return "Goodbye World..."
+
+if __name__ == "__main__":
+    app.run(debug=True)
